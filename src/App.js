3f3bbcfb9252d2ea362bbab956340435c9a31818
victory-nwawunze <victorynwawunze@gmail.com>
2022-08-09 06:27:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import React, { Fragment } from "react";
+import "./App.css";
 
-function App() {
+let App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Fragment>
+      <nav className="navbar navbar-dark bg-dark expand-sm">
+        <a href="/" className="navbar-brand">
+          React with Bootstrap styles
         </a>
-      </header>
-    </div>
+      </nav>
+    </Fragment>
   );
-}
+};
 
 export default App;
